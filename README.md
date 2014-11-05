988c47d366dd46abbeb3e04d86d28e11fbe1eacb
Jamie J Seol <theeluwin@gmail.com>
2014-11-06 04:30:00 +0900

    init

--- README.md
+++ README.md
@@ -1,4 +1,35 @@
-basehangul
-==========
+# BaseHangul
 
-Human-readable binary encoding for Lua
+Implementation of [BaseHangul](http://api.dcmys.jp/basehangul/) for Lua.
+
+## Usage & Examples
+
+##### Import basehangul module
+    
+    require("basehangul")
+
+##### Use it
+
+    print(basehangul.encode("This is an encoded string"))
+    -- 늠륄똔뜬더둘끙노닸땃뱀낍닳뒵갹뭡궂며또곰
+    
+    print(basehangul.decode("늠륄똔뜬더둘끙노닸땃뱀낍닳뒵갹뭡궂며또곰"))
+    -- This is an encoded string
+
+## License
+
+WTFPL(Do What the Fuck You Want to Public License)
+
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+Version 2, December 2004
+
+Copyright (C) 2014 Jamie Seol (theeluwin, theeluwin@gmail.com)
+
+Everyone is permitted to copy and distribute verbatim or modified
+copies of this license document, and changing it is allowed as long
+as the name is changed.
+
+DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE
+TERMS AND CONDITIONS FOR COPYING, DISTRIBUTION AND MODIFICATION
+
+0. You just DO WHAT THE FUCK YOU WANT TO.
---
+++ lib/basehangul.lua
@@ -0,0 +1,79 @@
+module(..., package.seeall)
+
+local ksc = "가각간갇갈갉갊감갑값갓갔강갖갗같갚갛개객갠갤갬갭갯갰갱갸갹갼걀걋걍걔걘걜거걱건걷걸걺검겁것겄겅겆겉겊겋게겐겔겜겝겟겠겡겨격겪견겯결겸겹겻겼경곁계곈곌곕곗고곡곤곧골곪곬곯곰곱곳공곶과곽관괄괆괌괍괏광괘괜괠괩괬괭괴괵괸괼굄굅굇굉교굔굘굡굣구국군굳굴굵굶굻굼굽굿궁궂궈궉권궐궜궝궤궷귀귁귄귈귐귑귓규균귤그극근귿글긁금급긋긍긔기긱긴긷길긺김깁깃깅깆깊까깍깎깐깔깖깜깝깟깠깡깥깨깩깬깰깸깹깻깼깽꺄꺅꺌꺼꺽꺾껀껄껌껍껏껐껑께껙껜껨껫껭껴껸껼꼇꼈꼍꼐꼬꼭꼰꼲꼴꼼꼽꼿꽁꽂꽃꽈꽉꽐꽜꽝꽤꽥꽹꾀꾄꾈꾐꾑꾕꾜꾸꾹꾼꿀꿇꿈꿉꿋꿍꿎꿔꿜꿨꿩꿰꿱꿴꿸뀀뀁뀄뀌뀐뀔뀜뀝뀨끄끅끈끊끌끎끓끔끕끗끙끝끼끽낀낄낌낍낏낑나낙낚난낟날낡낢남납낫났낭낮낯낱낳내낵낸낼냄냅냇냈냉냐냑냔냘냠냥너넉넋넌널넒넓넘넙넛넜넝넣네넥넨넬넴넵넷넸넹녀녁년녈념녑녔녕녘녜녠노녹논놀놂놈놉놋농높놓놔놘놜놨뇌뇐뇔뇜뇝뇟뇨뇩뇬뇰뇹뇻뇽누눅눈눋눌눔눕눗눙눠눴눼뉘뉜뉠뉨뉩뉴뉵뉼늄늅늉느늑는늘늙늚늠늡늣능늦늪늬늰늴니닉닌닐닒님닙닛닝닢다닥닦단닫달닭닮닯닳담답닷닸당닺닻닿대댁댄댈댐댑댓댔댕댜더덕덖던덛덜덞덟덤덥덧덩덫덮데덱덴델뎀뎁뎃뎄뎅뎌뎐뎔뎠뎡뎨뎬도독돈돋돌돎돐돔돕돗동돛돝돠돤돨돼됐되된될됨됩됫됴두둑둔둘둠둡둣둥둬뒀뒈뒝뒤뒨뒬뒵뒷뒹듀듄듈듐듕드득든듣들듦듬듭듯등듸디딕딘딛딜딤딥딧딨딩딪따딱딴딸땀땁땃땄땅땋때땍땐땔땜땝땟땠땡떠떡떤떨떪떫떰떱떳떴떵떻떼떽뗀뗄뗌뗍뗏뗐뗑뗘뗬또똑똔똘똥똬똴뙈뙤뙨뚜뚝뚠뚤뚫뚬뚱뛔뛰뛴뛸뜀뜁뜅뜨뜩뜬뜯뜰뜸뜹뜻띄띈띌띔띕띠띤띨띰띱띳띵라락란랄람랍랏랐랑랒랖랗래랙랜랠램랩랫랬랭랴략랸럇량러럭런럴럼럽럿렀렁렇레렉렌렐렘렙렛렝려력련렬렴렵렷렸령례롄롑롓로록론롤롬롭롯롱롸롼뢍뢨뢰뢴뢸룀룁룃룅료룐룔룝룟룡루룩룬룰룸룹룻룽뤄뤘뤠뤼뤽륀륄륌륏륑류륙륜률륨륩륫륭르륵른를름릅릇릉릊릍릎리릭린릴림립릿링마막만많맏말맑맒맘맙맛망맞맡맣매맥맨맬맴맵맷맸맹맺먀먁먈먕머먹먼멀멂멈멉멋멍멎멓메멕멘멜멤멥멧멨멩며멱면멸몃몄명몇몌모목몫몬몰몲몸몹못몽뫄뫈뫘뫙뫼묀묄묍묏묑묘묜묠묩묫무묵묶문묻물묽묾뭄뭅뭇뭉뭍뭏뭐뭔뭘뭡뭣뭬뮈뮌뮐뮤뮨뮬뮴뮷므믄믈믐믓미믹민믿밀밂밈밉밋밌밍및밑바박밖밗반받발밝밞밟밤밥밧방밭배백밴밸뱀뱁뱃뱄뱅뱉뱌뱍뱐뱝버벅번벋벌벎범법벗벙벚베벡벤벧벨벰벱벳벴벵벼벽변별볍볏볐병볕볘볜보복볶본볼봄봅봇봉봐봔봤봬뵀뵈뵉뵌뵐뵘뵙뵤뵨부북분붇불붉붊붐붑붓붕붙붚붜붤붰붸뷔뷕뷘뷜뷩뷰뷴뷸븀븃븅브븍븐블븜븝븟비빅빈빌빎빔빕빗"
+
+local function int2bin(data, luka)
+	local bin = ""
+	for i = 1, luka do
+		if data == 0 then
+			bin = "0" .. bin
+		else
+			bin = "" .. (data % 2) .. bin
+			data = math.floor(data / 2)
+		end
+	end
+	return bin
+end
+
+local function asc2bin(data)
+	local bin = ""
+	local asc_list = {string.byte(data, 0, -1)}
+	for i = 1, #asc_list do
+		bin = bin .. int2bin(asc_list[i], 8)
+	end
+	while string.len(bin) % 40 ~= 0 do
+		bin = bin .. "0"
+	end
+	return bin
+end
+
+local function bin2hangul(data)
+	local hangul = ""
+	local data_length = string.len(data) / 10
+	for i = 1, data_length do
+		local index = tonumber(string.sub(data, (i - 1) * 10 + 1, i * 10), 2)
+		if index == 0 then
+			if i == data_length then
+				hangul = hangul .. "빎"
+			else
+				hangul = hangul .. "흐"
+			end
+		else
+			hangul = hangul .. string.sub(ksc, index * 3 + 1, (index + 1) * 3)
+		end
+	end
+	return hangul
+end
+
+local function hangul2bin(data)
+	local bin = ""
+	for i = 1, string.len(data) / 3 do
+		local hangul = string.sub(data, (i - 1) * 3 + 1, i * 3)
+		local index = string.find(ksc, hangul)
+		local chunk
+		if index == nil or index == 3073 then
+			chunk = "0000000000"
+		else
+			chunk = int2bin((index - 1) / 3, 10)
+		end
+		bin = bin .. chunk
+	end
+	return bin
+end
+
+local function bin2asc(data)
+	local asc = ""
+	for i = 1, string.len(data) / 8 do
+		local dec = tonumber(string.sub(data, (i - 1) * 8 + 1, i * 8), 2)
+		asc = string.format("%c", dec) .. asc
+	end
+	return asc
+end
+
+function encode(data)
+	return bin2hangul(asc2bin(data))
+end
+
+function decode(data)
+	return bin2asc(hangul2bin(data))
+end
